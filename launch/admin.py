e8c03125c76d9e0e01b6be9f62f5af34a571c710
ibrahim baba jr <ibress1234@gmail.com>
2026-06-06 16:22:16 +0100

    ready for deploy

--- launch/admin.py
+++ launch/admin.py
@@ -1,16 +1,5 @@
 from django.contrib import admin
 from .models import Feedback, Waitlist
 
-
-@admin.register(Waitlist)
-class WaitlistAdmin(admin.ModelAdmin):
-    list_display = ("email", "created_at")
-    search_fields = ("email",)
-    ordering = ("-created_at",)
-
-
-@admin.register(Feedback)
-class FeedbackAdmin(admin.ModelAdmin):
-    list_display = ("name", "email", "created_at")
-    search_fields = ("name", "email")
-    ordering = ("-created_at",)
+admin.site.register(Feedback)
+admin.site.register(Waitlist)
