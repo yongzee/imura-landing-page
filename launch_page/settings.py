e6e7b6f879e57da32359fa3af065887534a3051c
ibrahim baba jr <ibress1234@gmail.com>
2026-06-06 16:39:41 +0100

    ready for deploy

--- launch_page/settings.py
+++ launch_page/settings.py
@@ -15,6 +15,8 @@ import os
 
 import dj_database_url
 
+if os.environ.get("RENDER"):
+    DEBUG = False
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,7 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "launch"
+    "launch",
 ]
 
 MIDDLEWARE = [
