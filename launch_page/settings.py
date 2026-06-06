1acd484980229615c838587879fd802f4a3e758e
ibrahim baba jr <ibress1234@gmail.com>
2026-06-06 15:07:59 +0100

    ready for deploy

--- launch_page/settings.py
+++ launch_page/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
+import dj_database_url
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,11 +80,9 @@ WSGI_APPLICATION = "launch_page.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.parse('postgresql://imura_landing_page_user:GBroVLZkucbrXgbgyPq0r4MK3Ng4KcXj@dpg-d8i2fdlckfvc73bc4qm0-a.oregon-postgres.render.com/imura_landing_page')
 }
+
 
 
 # Password validation
